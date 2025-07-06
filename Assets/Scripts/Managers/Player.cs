86f208ec6fe198697ab7d7aa8aca9fbf0d03547f
Xenawn <codeparadoxist@gmail.com>
2025-07-06 17:33:59 +0900

    [Feat] Singleton Manager

---
+++ Assets/Scripts/Managers/Player.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private void Start()
+    {
+        Managers mg = Managers.Instance;
+    }
+    
+}
