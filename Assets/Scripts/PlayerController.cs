bb9915b5efc7f4d7262a94353e660a2f9491bddc
Xenawn <codeparadoxist@gmail.com>
2025-07-21 21:10:53 +0900

    [Update] 구조체 Vector 연습

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,7 +1,40 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+// 1.위치 벡터
+// 2.방향 벡터
+struct MyVector
+{
+    public float x;
+    public float y;
+    public float z;
+   
+    public float manitude { get { return Mathf.Sqrt(x*x+y*y+z*z); } }
+    public MyVector normalized { get { return new MyVector(x / manitude, y/manitude, z/manitude); } }   
+    public MyVector(float x, float y, float z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+    public static MyVector operator +(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x + b.x, a.y + b.y, a.z + b.z);
+    }
+
+    public static MyVector operator -(MyVector a, MyVector b)
+    {
+        return new MyVector(a.x - b.x, a.y - b.y, a.z - b.z);
+    }
+
+    public static MyVector operator *(MyVector a, float d)
+    {
+        return new MyVector(a.x *d, a.y *d, a.z *d);
+    }
+
+}
 public class PlayerController : MonoBehaviour
 {
     // GameObject (Player)  
@@ -10,7 +43,20 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField]
     float _speed = 10.0f;
-    public GameObject _obj;
+
+
+    private void Start()
+    {
+        MyVector to = new MyVector(10, 0, 0);
+        MyVector from = new MyVector(5, 0, 0);
+        MyVector dir = to - from;
+        dir = dir.normalized;
+
+        MyVector newPos = from + dir * _speed;
+        // 방향 벡터
+            // 1. 거리(크기) 5 normalized
+            // 2. 실제 방향 ->
+    }
     void Update()
     {
        // Local => World
