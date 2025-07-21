57cd29d66904c6a9b489cf65d09444ac65efbc21
Xenawn <codeparadoxist@gmail.com>
2025-07-21 16:19:06 +0900

    [Feat] 이동 코드 변경

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -13,22 +13,27 @@ public class PlayerController : MonoBehaviour
     public GameObject _obj;
     void Update()
     {
-       
+       // Local => World
+       // TransformDirection
+
+        //World => Local
+        // InverseTransformDirection
         if (Input.GetKey(KeyCode.W))
         {
-            transform.position += Vector3.forward * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.forward * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.S))
         {
-            transform.position += new Vector3(0, 0, -1) * Time.deltaTime * _speed;
+            
+            transform.position += transform.TransformDirection(Vector3.back * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.A))
         {
-            transform.position += new Vector3(-1, 0, 0) * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.left * Time.deltaTime * _speed);
         }
         if (Input.GetKey(KeyCode.D))
         {
-            transform.position += new Vector3(1, 0, 0) * Time.deltaTime * _speed;
+            transform.position += transform.TransformDirection(Vector3.right * Time.deltaTime * _speed);
         }
     }
 }
