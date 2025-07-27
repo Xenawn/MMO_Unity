21a9dd01697541c3e42d0d633acb3378d010775f
Xenawn <codeparadoxist@gmail.com>
2025-07-27 16:08:38 +0900

    [Study] Raycast2

--- Assets/Scripts/TestCollision.cs
+++ Assets/Scripts/TestCollision.cs
@@ -28,19 +28,38 @@ public class TestCollision : MonoBehaviour
     private void Update()
     {
         // Local <=> World<=> Viewport <=> Screen
-        Vector3 look = transform.TransformDirection(Vector3.forward);
-        Debug.DrawRay(transform.position+Vector3.up, look*10, Color.red);
-       
-        RaycastHit[] hits;
-        hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
-        foreach(RaycastHit hit in hits)
+        // Debug.Log(Input.mousePosition); // 마우스 좌표 = Screen 픽셀
+
+        //Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition )); // 픽셀을 비율화
+        if (Input.GetMouseButton(0))
         {
-            Debug.Log($"Raycast {hit.collider.gameObject.name}");
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            
+
+            Debug.DrawRay(Camera.main.transform.position, ray.direction, Color.red, 1.0f);
+            RaycastHit hit;
+            
+            if (Physics.Raycast(ray, out hit, 100.0f))
+            {
+                Debug.Log($"RayCast Camera @ {hit.collider.gameObject.name}");
+            }
         }
-        //RaycastHit hit;
-        //if (Physics.Raycast(transform.position+Vector3.up,look,out hit, 10))
-        //{
-        //    Debug.Log($"RayCast{hit.collider.gameObject.name}");
-        //}
+
+        /*if (Input.GetMouseButton(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition ); // 아래 3줄을 이 한줄로 끝
+            //Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
+            //Vector3 dir = mousePos - Camera.main.transform.position;
+            //dir = dir.normalized;
+
+            RaycastHit hit;
+
+            Debug.DrawRay(Camera.main.transform.position, dir, Color.red, 1.0f);
+            if (Physics.Raycast(Camera.main.transform.position, dir, out hit))
+            {
+                Debug.Log($"RayCast Camera @ {hit.collider.gameObject.name}");
+            }
+        }
+       */
     }
 }
